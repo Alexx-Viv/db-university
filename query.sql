28895c4865f5d8798d9b9c176b588b2d9de2283c
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-28 14:16:01 +0200

    query 7

--- query.sql
+++ query.sql
@@ -21,3 +21,7 @@ SELECT * FROM `exams` WHERE HOUR(`hour`) >= 14 AND DATE(`date`) = '2020-06-20'
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
 SELECT * FROM `degrees` WHERE `level` = 'magistrale'
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+
+SELECT * FROM `departments`
