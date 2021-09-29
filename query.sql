b54cc0bb6de217148be939fb1d7163d26d9bd267
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-29 14:27:08 +0200

    Query 5 Join

--- query.sql
+++ query.sql
@@ -73,5 +73,16 @@ ON `degrees`.`department_id` = `departments`.`id`
 ORDER BY `students`.`surname`, `students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT `degrees`.`name` AS 'Corso di Laurea', `courses`.`name` AS 'Corso', `teachers`.`name` AS 'Nome insegnante',`teachers`.`surname` AS 'Cognome insegnante'
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id`=`courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+ORDER BY `degrees`.`name`;
+
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
