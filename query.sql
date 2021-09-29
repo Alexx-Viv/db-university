6a88e0c0f22308dd588415ff22de1cfca30bb6ce
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-29 14:13:49 +0200

    query 3 join

--- query.sql
+++ query.sql
@@ -59,6 +59,9 @@ SELECT * FROM `students` JOIN `degrees` ON `students`.`degree_id` = `degrees`.`i
 SELECT * FROM `degrees` JOIN `departments` ON `degrees`.`department_id`= `departments`.`id` WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `courses`.`name` FROM `courses` JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id` JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id` WHERE `teachers`.`id` = 44;
+
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
